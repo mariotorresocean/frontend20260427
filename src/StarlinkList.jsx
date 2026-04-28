df70715c37d74d4f25ee19e1a2592b0ec7c5efcc
Prof. Mário Torres <mario.torres@oceanbrasil.com>
2026-04-27 20:03:41 -0400

    lista de satelites

--- src/StarlinkList.jsx
+++ src/StarlinkList.jsx
@@ -1,4 +1,27 @@
+import { useEffect, useState } from "react"
+
 export default function StarlinkList() {
-    return <h1>Lista de satelites</h1>
+    const [starlinks, setStarlinks] = useState([]);
+    useEffect(() => {
+        fetch('https://api.spacexdata.com/v4/starlink/query', {
+            method: 'POST',
+            body: {
+            "query": {},
+            "options": { limit: 20 }
+            }
+        })
+        .then((response) => response.json())
+        .then((data) => setStarlinks(data.docs))
+        .catch((error) => console.error('Deu bug:', error))
+    }, [])
+
+    return <>
+        <h1>Lista de satelites</h1>
+        <ul>
+            {starlinks.map((s) => (
+                <li>{s.spaceTrack.OBJECT_NAME}</li>
+            ))}
+        </ul>
+    </>
 }
 
